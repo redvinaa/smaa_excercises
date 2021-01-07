27d537fb9cae3687d12ed224d13d4bb6a8dcd018
Vince Reda <redvinaa@gmail.com>
2021-01-07 14:02:40 +0100

    Fixup

--- 2-Kernel_methods/ex_2.py
+++ 2-Kernel_methods/ex_2.py
@@ -15,19 +15,19 @@ a__class = np.random.choice([-1, 1], size=p__n)
 a__points = np.empty(shape=(p__n, 2))
 for i, c in enumerate(a__class):
 	if c == 1:
-		a__points[i] = p__cov_a @ np.random.standard_normal(size=2)
+		a__points[i] = p__cov_a @ np.random.standard_normal(size=2) + p__mean
 	else:
 		r     = p__R + np.random.standard_normal()*p__cov_b
 		theta = np.random.uniform(low=0, high=2*np.pi)
 
-		a__points[i] = np.array([r*np.cos(theta), r*np.sin(theta)])
+		a__points[i] = np.array([r*np.cos(theta), r*np.sin(theta)]) + p__mean
 
 ## }}}
 
 
 ## b) Kernel implementation {{{
 
-def kernel(x, y, mode='gaussian', params=None):
+def kernel(x, y, mode=p__mode, params=p__params):
 	if mode=='linear':
 		return x @ y
 	if mode=='polynomial':
@@ -52,38 +52,63 @@ def kernel(x, y, mode='gaussian', params=None):
 
 X = a__points
 Y = a__class
-
-n = p__n
-lambd = p__lambd
-
-a_p = cp.Variable(n, nonneg=True)
-a_n = cp.Variable(n, nonneg=True)
+n     = p__n
 
 K = np.empty(shape=(n, n))
+K_Y = np.empty(shape=(n, n))
 for i in range(n):
 	for j in range(n):
-		K[i, j] = kernel(X[i], X[j], mode=p__mode, params=p__params)
+		K[i, j] = kernel(X[i], X[j])
+		K_Y[i, j] = K[i, j] * Y[i] * Y[j]
 
-obj = Y @ (a_p-a_n) - 1/2 * cp.pos((a_p-a_n)) @ cp.pos((a_p-a_n)) - p__eps * cp.sum(a_p+a_n)
+def fun(alpha):
+	eq = np.sum(alpha) - 1/2 * np.sum([alpha[k]*alpha[m]*Y[k]*Y[m]*K[k,m] \
+		for k in range(n) for m in range(n)])
+	return -eq
 
-eq1 = cp.sum([(a_p[i]-a_n[i])**2 for i in range(n)])
+from scipy.optimize import minimize, Bounds, LinearConstraint
 
-eq = (a_p-a_n) @ K @ (a_p-a_n)
-obj = Y @ (a_p-a_n) - 1/2 * eq1 - p__eps * cp.sum(a_p+a_n)
+bounds = Bounds(np.zeros((n,)), np.full((n,), np.inf))
+constraints = LinearConstraint(Y, 0, 0)
+res = minimize(fun, np.zeros((n,)), bounds=bounds, constraints=constraints)
 
-constr = [a <= lambd for a in np.append(a_p, a_n)]
-constr.append(cp.sum(a_p-a_n) == 0)
+if not res.success:
+	quit('ERROR: ' + res.message)
 
-prob = cp.Problem(cp.Maximize(obj), constr)
-prob.solve()
+alpha = res.x
+print(f'alpha: {alpha}')
 
-a_p = a_p.value
-a_n = a_n.value
-w = (a_p-a_n) @ X
+idx = np.argwhere(alpha!=0).flatten()[0]
+print(f'idx = {idx}')
 
-s_b = 0 # TODO
-def f_optimal(x):
-	return np.sum([(a_p_k-a_n_k) @ kernel(x, x_k) for a_p_k, a_n_k, x_k in zip(a_p, a_k, X)]) + s_b
+w = np.sum([a*y*x for a, y, x in zip(alpha, Y, X)], axis=0)
+print(f'w = {w}')
+
+x = X[idx]
+y = Y[idx]
+b = y - kernel(w, x)
+
+print(f'b = {b}')
+
+#  alpha_outer = cp.vstack([alpha*a for a in alpha])
+#  outer       = cp.multiply(alpha_outer, K_Y)
+#  obj         = cp.sum(alpha) - 1/2*eq
+#  obj         = cp.Maximize(obj)
+## ^^ this is how it would be done if cvxpy wasn't a piece of shit
+
+#  constr = [lambd >= alpha[k] for k in range(n)]
+#  constr = [alpha[k] >= 0 for k in range(n)]
+#  constr.append(cp.sum([alpha[k]*Y[k] for k in range(n)]) == 0)
+
+#  prob = cp.Problem(obj, constr)
+#  prob.solve()
+
+#  try:
+#      prob.solve()
+#  except cp.error.DCPError:
+#      print('DCPERROR')
+#  quit()
+
 
 ## }}}
 
@@ -100,6 +125,17 @@ pts_b_y = a__points[a__class==-1][:,1]
 plt.scatter(pts_a_x, pts_a_y, label='Sample: class a', color='black')
 plt.scatter(pts_b_x, pts_b_y, label='Sample: class b', \
 	edgecolors='black', facecolors='none')
+
+n = 100
+X = np.random.uniform(-10, 20, size=2*n).reshape((-1, 2))
+Y = np.array([kernel(w, x) + b for x in X])
+
+pts_a_x = X[Y>=0][:, 0]
+pts_a_y = X[Y>=0][:, 1]
+pts_b_x = X[Y<0][:, 0]
+pts_b_y = X[Y<0][:, 1]
+plt.scatter(pts_a_x, pts_a_y, color='red', alpha=.3, label='Class a')
+plt.scatter(pts_b_x, pts_b_y, color='blue', alpha=.3, label='Class b')
 
 plt.legend(loc=loc)
 plt.grid()
--- 2-Kernel_methods/ex_2_param.py
+++ 2-Kernel_methods/ex_2_param.py
@@ -2,16 +2,12 @@
 
 from numpy import array
 
-p__n = 100
+p__n = 30
 p__R = 3
 
+p__mean = array([5, 5])
 p__cov_a = array([[1, 0], [0, 1]])
 p__cov_b = .3
 
-p__eps = .1
-p__lambd = 1
-
-#  p__mode = 'gaussian'
-#  p__params = {'sigma': 1}
-p__mode = 'linear'
-p__params = {}
+p__mode = 'gaussian'
+p__params = {'sigma': 1}
